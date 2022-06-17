88304f8fa33c964027434814671d82dc460f2cc8
Connor Adams <cleeadacon909@gmail.com>
2022-06-16 20:51:09 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from arithmetic_arranger import *
 
-problem = ['2 - 2134', '4 + 1235', '45 + 405', '62 - 30']
+problem = ['2 + 213', '4 + 1235', '45 + 405', '62 - 30']
 arithmetic_arranger(problem, True)
+
+print('Hello Dick Head')
